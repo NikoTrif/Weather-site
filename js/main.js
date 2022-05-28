ba21204a6fdfde8db3138f57df957e8d4ba482a8
NikoTrif <60791847+NikoTrif@users.noreply.github.com>
2022-05-29 01:08:58 +0200

    Postavlja se temperatura kada se selektuje grad

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
 let place = document.getElementById("place");
 let searcher = document.getElementById("searcher");
 let selectCity = document.getElementById("citySelect");
+let usedUnit = document.querySelector('#used-unit');
+let unusedUnit = document.querySelector('#unused-unit a');
+let currentTemp = document.querySelector('#current-temp');
 let selectOption;
-let searchVal = '';
 let apiKey = '8e1cf7d0e536d5b4b9e3ab3ff361aaf5';
-let lat = 44.81;
-let lon = 20.46;
 let city;
 let cities;
-//let apiPath = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+let weatherData;
 
-async function LoadWeather() {
-    let weatherData = await fetch(apiPath);
-    let dat = await weatherData.json();
+// console.log(currentTemp.textContent);
 
-    console.log("Load Weather -> dat:");
-    console.log(dat);
+let temp;
+let weather;
+let feel;
+let wind;
+let preasure;
+let visibility;
+let humidity;
+
+async function LoadWeather(lat, lon) {
+    let apiPath = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    weatherData = await (await fetch(apiPath)).json();
+
+    console.log("Load Weather -> weatherData:");
+    console.log(weatherData);
 }
 
 async function LoadCities() {
@@ -24,63 +34,64 @@ async function LoadCities() {
     cities = await jsonconn.json();
     console.log("LOAD CITIES -> cities:");
     console.log(cities);
-
-    // city = await cities.filter(e => e.city == 'Belgrade')
-
-    // console.log("Load Cities -> City:");
-    // console.log(city);
-    // console.log("Load Cities -> Coordinates:");
-    // console.log(`Latitude: ${city[0].lat} / Longitude: ${city[0].lng}`);
-
 }
+
+function UnitConvert(tempVal, unit) {
+    let tempConverted;
+
+    switch (unit) {
+        case 'C':
+            tempConverted = Math.floor(tempVal - 273.15);
+            break;
+        case 'F':
+            tempConverted = Math.floor((tempVal - 273.15) * (9 / 5) + 32);
+            break;
+
+        default:
+            tempConverted = 'ERROR';
+            break;
+    }
+    console.log('UNIT CONVERT -> tempConverted');
+    console.log(`${tempConverted} ${unit}`);
+    return tempConverted;
+}
+
 LoadCities();
-LoadWeather();
+LoadWeather(44.81, 20.46);
 
 place.addEventListener('click', function(e) {
     e.preventDefault();
 
-    searcher.classList.replace('d-none', 'd-block');
+    searcher.classList.toggle('d-none');
+    searcher.classList.toggle('d-block');
     searcher.focus();
 });
 
 searcher.addEventListener('keypress', function(e) {
     if (e.key === 'Enter') {
-        //change city and state
         console.log('enter');
         console.log(searcher.value);
-        searchVal = searcher.value;
+        selectCity.selectedIndex = 0;
+        selectCity.click();
         searcher.classList.replace('d-block', 'd-none');
-
-        //let flt = cities.properties.name.filter(e => e == searchVal);
+        searcher.value = null;
     }
-    // else {
-    //     console.log(searcher.value);
-    //     city = cities.filter(ea => ea.city = searcher.value)
-    //     if (city > 0) {
-    //         selectCity.classList.replace('d-none', 'd-block');
-
-
-    //         city.forEach(eb => {
-    //             selectCity.innerHTML = selectCity.innerHTML + `<option>${eb.city}, ${eb.country}</option>`;
-    //         });
-    //     }
-    // }
 });
 
 searcher.addEventListener('keyup', async function() {
-    // console.log(searcher.value);
-    //city = await cities.filter(ea => ea.city == searcher.value)
     city = await cities.filter(ea => {
-        return ea.city.includes(searcher.value);
+        return ea.city.toLowerCase().includes(searcher.value.toLowerCase());
     })
-    console.log(`Searcher -> KeyUP -> City:`);
+    console.log(`Searcher - > KeyUP - > City: `);
     console.log(city);
     selectCity.innerHTML = '';
     if (city.length > 0 && city.length < 10) {
         selectCity.classList.replace('d-none', 'd-block');
+        selectCity.setAttribute('size', city.length);
+        selectCity.classList.remove('fomr-select');
 
         city.forEach(eb => {
-            selectCity.innerHTML = selectCity.innerHTML + `<option value="${eb.lat},${eb.lng}">${eb.city}, ${eb.country}</option>`;
+            selectCity.innerHTML = selectCity.innerHTML + `<option value = "${eb.lat},${eb.lng}"> ${eb.city}, ${eb.country}</option>`;
         });
         selectOption = document.getElementsByTagName('option');
     } else {
@@ -89,6 +100,30 @@ searcher.addEventListener('keyup', async function() {
     }
 });
 
-selectCity.addEventListener('click', () => {
+selectCity.addEventListener('click', async() => {
+    let coordinates = selectCity.value.split(',');
+    console.log(selectCity.value);
+
+    await LoadWeather(coordinates[0], coordinates[1]);
+
+    selectCity.classList.replace('d-block', 'd-none');
+    searcher.classList.replace('d-block', 'd-none');
+    searcher.value = null;
+
+    place.innerHTML = selectCity.options[selectCity.selectedIndex].textContent;
+
+    //Setting Weather Data
+    currentTemp.textContent = UnitConvert(weatherData.main.temp, usedUnit.textContent);
     //ovde sam stao
+});
+
+unusedUnit.addEventListener('click', (e) => {
+    e.preventDefault();
+    let uu = usedUnit.textContent;
+    let unu = unusedUnit.textContent;
+
+    usedUnit.textContent = unu;
+    unusedUnit.textContent = uu;
+
+    currentTemp.textContent = UnitConvert(weatherData.main.temp, usedUnit.textContent);
 });
